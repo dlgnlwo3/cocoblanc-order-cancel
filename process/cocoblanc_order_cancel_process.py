cfeec2e0e52acaf11357663b0160b19e2f036756
dlgnlwo3 <dlgnlwo3@naver.com>
2023-07-04 14:13:42 +0900

    0.0.6 -> 전체적인 검증 과정 수정

--- process/cocoblanc_order_cancel_process.py
+++ process/cocoblanc_order_cancel_process.py
@@ -91,33 +91,33 @@ class CocoblancOrderCancelProcess:
                         ezadmin.login()
                         self.cs_screen_tab = ezadmin.switch_to_cs_screen()
 
-                    # if account == "카카오톡스토어":
-                    #     kakaotalk_store = KakaoTalkStore(self.log_msg, self.driver, self.cs_screen_tab, dict_account)
-                    #     kakaotalk_store.work_start()
+                    if account == "카카오톡스토어":
+                        kakaotalk_store = KakaoTalkStore(self.log_msg, self.driver, self.cs_screen_tab, dict_account)
+                        kakaotalk_store.work_start()
 
-                    # if account == "위메프":
-                    #     wemakeprice = Wemakeprice(self.log_msg, self.driver, self.cs_screen_tab, dict_account)
-                    #     wemakeprice.work_start()
+                    if account == "위메프":
+                        wemakeprice = Wemakeprice(self.log_msg, self.driver, self.cs_screen_tab, dict_account)
+                        wemakeprice.work_start()
 
-                    # if account == "티몬":
-                    #     ticketmonster = TicketMonster(self.log_msg, self.driver, self.cs_screen_tab, dict_account)
-                    #     ticketmonster.work_start()
+                    if account == "티몬":
+                        ticketmonster = TicketMonster(self.log_msg, self.driver, self.cs_screen_tab, dict_account)
+                        ticketmonster.work_start()
 
-                    # if account == "지그재그":
-                    #     zigzag = Zigzag(self.log_msg, self.driver, self.cs_screen_tab, dict_account)
-                    #     zigzag.work_start()
+                    if account == "지그재그":
+                        zigzag = Zigzag(self.log_msg, self.driver, self.cs_screen_tab, dict_account)
+                        zigzag.work_start()
 
-                    # if account == "브리치":
-                    #     bflow = Bflow(self.log_msg, self.driver, self.cs_screen_tab, dict_account)
-                    #     bflow.work_start()
+                    if account == "브리치":
+                        bflow = Bflow(self.log_msg, self.driver, self.cs_screen_tab, dict_account)
+                        bflow.work_start()
 
-                    # if account == "쿠팡":
-                    #     coupang = Coupang(self.log_msg, self.driver, self.cs_screen_tab, dict_account)
-                    #     coupang.work_start()
+                    if account == "쿠팡":
+                        coupang = Coupang(self.log_msg, self.driver, self.cs_screen_tab, dict_account)
+                        coupang.work_start()
 
-                    # if account == "11번가":
-                    #     eleven_street = ElevenStreet(self.log_msg, self.driver, self.cs_screen_tab, dict_account)
-                    #     eleven_street.work_start()
+                    if account == "11번가":
+                        eleven_street = ElevenStreet(self.log_msg, self.driver, self.cs_screen_tab, dict_account)
+                        eleven_street.work_start()
 
                     if account == "네이버":
                         naver = Naver(self.log_msg, self.driver, self.cs_screen_tab, dict_account)
